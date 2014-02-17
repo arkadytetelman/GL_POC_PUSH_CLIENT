b5cd4a6ca9727e5a72349677f0cd61dee28d20cd
arkadii.tetelman <tetelman@ukr.net>
2014-02-17 17:22:36 +0200

    Initial commit

---
+++ GL_POC_PUSH_CLIENT/src/main/java/com/example/gl_poc_push_client/BroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.example.gl_poc_push_client;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class BroadcastReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that PushService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                PushService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
